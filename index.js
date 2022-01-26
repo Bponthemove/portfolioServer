81fcea4efb4fbacfce371bc0612ffe26b453932a
Bponthemove <bpvanzalk73@gmail.com>
2022-01-26 12:17:51 +0000

    update

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'client/public')))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 app.use(cors())
-app.use(cors({ origin: 'https://kind-shaw-87f836.netlify.app/' }))
+
 
 /////////////////////////routes//////////////////////////////
 
