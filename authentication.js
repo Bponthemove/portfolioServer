9369bb0d086e98ee0a00c7a1fe5ce92aa98f399e
Bponthemove <bpvanzalk73@gmail.com>
2022-02-01 14:58:01 +0000

    cl

--- authentication.js
+++ authentication.js
@@ -6,18 +6,23 @@ module.exports = (req, res, next) => {
         // Read the JWT access token from the request header
     const accessToken = req.headers.authorization.replace("Bearer ", "")
     const { accesstype: accessType, accessfor: accessFor } = req.headers
+    console.log(12)
+    console.log(req.headers)
     
         // Verify the token using the Userfront public key 
         //  use .replace(/\\n/gm, '\n') and \n before and after the string in the env file
     try {
+        console.log(13)
         const verifiedPayload = jwt.verify(
             accessToken,
             process.env.USERFRONT_PUBLIC_KEY.replace(/\\n/gm, '\n'),
             { algorithms: ["RS256"] }
         )
+        console.log(verifiedPayload)
         //check whether access is authorized for certain action (eg member auth for member access and admin auth for admin access)
         const accessRole = verifiedPayload.authorization[process.env.USERFRONT_TENANT_ID] || {}
         if ((accessType === 'admin' & accessRole.roles.includes('admin')) || (accessType === 'member' & accessRole.roles.includes('member'))) {
+            console.log(15)
             req.auth = { verifiedPayload, accessFor, accessToken }
             next()
         } else {
